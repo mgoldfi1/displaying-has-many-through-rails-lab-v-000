bb7c1dcda0f571f47ba2eb5995c45e5d574caa4e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-22 16:35:13 +0000

    automatically backed up by learn

--- db/migrate/20180822163419_change_appointment_date_time_back.rb
+++ db/migrate/20180822163419_change_appointment_date_time_back.rb
@@ -2,5 +2,5 @@ class ChangeAppointmentDateTimeBack < ActiveRecord::Migration
   def change
       change_column :appointments, :appointment_datetime, :string
     end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822162858) do
+ActiveRecord::Schema.define(version: 20180822163419) do
 
   create_table "appointments", force: :cascade do |t|
-    t.date    "appointment_datetime"
+    t.string  "appointment_datetime"
     t.integer "doctor_id"
     t.integer "patient_id"
   end
