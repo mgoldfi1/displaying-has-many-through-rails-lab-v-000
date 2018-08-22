ebbf253ce44ea9595b00c8f9a307d898affd3a49
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-22 15:52:28 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822153524) do
+ActiveRecord::Schema.define(version: 20180822155155) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.string  "appointment_datetime"
+    t.integer "doctor_id"
+    t.integer "patient_id"
+  end
+
+  add_index "appointments", ["doctor_id"], name: "index_appointments_on_doctor_id"
+  add_index "appointments", ["patient_id"], name: "index_appointments_on_patient_id"
 
   create_table "doctors", force: :cascade do |t|
     t.string   "name"
